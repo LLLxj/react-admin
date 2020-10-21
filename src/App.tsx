58b21cdffff97657f3c090f8510231ba8b8add85
liuzijian <liuzijian@gaohangip.com>
2020-10-21 10:10:39 +0800

    更新配置文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+// import NavBar from ''
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Layout>
+        <Layout>
+          {/* <NavBar></NavBar>
+          <SideBar></SideBar>
+          <Content></Content> */}
+        </Layout>
+      </Layout>
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
